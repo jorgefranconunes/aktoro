0f85205453d81c8e6439719db0da316fb9c5bd2a
jorgefranconunes <jorgefranconunes@gmail.com>
2017-09-23 10:07:48 +0200

    Refs #26 Added reference to Javadocs API in project site

--- modules/api/src/main/java/com/varmateo/aktoro/ActorRef.java
+++ modules/api/src/main/java/com/varmateo/aktoro/ActorRef.java
@@ -17,7 +17,7 @@ package com.varmateo.aktoro;
  *
  * <p>When an actor is created an <code>ActorRef</code> instance is
  * passed to its factory (see <code>{@link
- * ActorSystem#createActor(ActorCoreFactory,Class)}</code>. The actor
+ * ActorSystem#createActor(ActorCoreFactory,Class)}</code>). The actor
  * can then use that <code>ActorRef</code> to execute lambdas, by
  * calling <code>{@link #perform(Runnable)}</code>, or to pass
  * references of itself to outside, by calling <code>{@link
